1d556c29c1a3c32d9592c4128a549d0c2e13a323
Amith B V <amithrao0606@gmail.com>
2025-12-22 19:46:29 +0530

    Installed and setup "@elysia/eden", for end-to-end type safety similar to tRPC.

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,8 +1,8 @@
 import { Elysia, t } from "elysia";
 
-const app = new Elysia({ prefix: "/api" })
+export const app = new Elysia({ prefix: "/api" })
   .get("/", "Hello Nextjs")
-  .get("/users", { user: { name: "Amith" } });
+  .get("/users", { user: { name: "Amith", age: 25 } });
 
 export const GET = app.fetch;
 export const POST = app.fetch;
