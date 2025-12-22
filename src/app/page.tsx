bf38e24bf9ae1589b04068d467c9a60067b0f4c7
Amith B V <amithrao0606@gmail.com>
2025-12-22 07:20:40 +0530

    Created a basic UI for user to create a room.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [username, setUsername] = useState("Amith");
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Hello World!!</h1>
-    </div>
+    <main className="flex min-h-screen flex-col justify-center items-center p-4">
+      <div className="w-full max-w-md space-y-">
+        {/* The box where inputs will be there : */}
+        <div className="border border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md">
+          <div className="space-y-5">
+            <div className="space-y-2">
+              <label className="flex items-center text-zinc-500">
+                Your Identity
+              </label>
+
+              <div className="flex items-center gap-3">
+                <div className="flex-1 bg-zinc-950 border border-zinc-800 p-3 text-sm text-zinc-400 font-mono">
+                  {username}
+                </div>
+              </div>
+            </div>
+
+            <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">
+              Create room
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
