c749b6fff5982af7904b0efa64faa9b591590303
Amith B V <amithrao0606@gmail.com>
2025-12-23 12:53:20 +0530

    Moved the chat creation screen to different route so that I can have a landing page for this chat application in future.

---
+++ src/app/create-room/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { api } from "@/lib/eden";
+import { useMutation } from "@tanstack/react-query";
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+
+const ANIMALS = ["lion", "wolf", "hawk", "bear", "tiger", "horse", "huskey"];
+const STORAGE_KEY = "afterten_username";
+
+const generateUsername = () => {
+  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)];
+  return `anonymous-${word}-${nanoid(5)}`;
+};
+
+export default function Home() {
+  const [username, setUsername] = useState("");
+
+  const { mutate: createRoom } = useMutation({
+    mutationFn: async () => {
+      const res = await api.room.create.post();
+    },
+  });
+
+  useEffect(() => {
+    const main = () => {
+      const stored = localStorage.getItem(STORAGE_KEY);
+
+      if (stored) {
+        setUsername(stored);
+        return;
+      }
+
+      const generated = generateUsername();
+      localStorage.setItem(STORAGE_KEY, generated);
+      setUsername(generated);
+    };
+
+    main();
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col justify-center items-center p-4">
+      <div className="w-full max-w-md space-y-8">
+        {/* App name text : */}
+        <div className="text-center space-y-2">
+          <h1 className="text-2xl font-bold tracking-tight text-green-500">
+            {">"}private_chat
+          </h1>
+
+          <p className="text-zinc-500 text-sm">
+            A private, self-destructing chat room.
+          </p>
+        </div>
+
+        {/* The box where inputs will be there : */}
+        <div className="border border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md">
+          <div className="space-y-5">
+            <div className="space-y-2">
+              <label className="flex items-center text-zinc-500">
+                Your Identity
+              </label>
+
+              <div className="flex items-center gap-3">
+                <div className="flex-1 bg-zinc-950 border border-zinc-800 p-3 text-sm text-zinc-400 font-mono">
+                  {username}
+                </div>
+              </div>
+            </div>
+
+            <button
+              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50"
+              onClick={() => createRoom()}
+            >
+              Create room
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
