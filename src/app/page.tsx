c5783fb64bb947afa5bf174609af48649e3b81b9
Amith B V <amithrao0606@gmail.com>
2026-01-15 13:35:24 +0530

    Added button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-center items-center p-4">
@@ -11,6 +13,12 @@ export default function Home() {
           <p className="text-zinc-500 text-sm">
             A private, secure & self-destructing chat room.
           </p>
+
+          <Link href={"/create-room"}>
+            <button className="bg-zinc-300 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50 rounded">
+              Create room
+            </button>
+          </Link>
         </div>
       </div>
     </main>
